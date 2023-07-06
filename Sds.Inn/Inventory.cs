97ba03aadca73e8955c326c411d0548f4d132f2c
Eric Smith <Eric.Smith@sds.io>
2023-07-05 21:28:33 -0400

    Initial commit

---
+++ Sds.Inn/Inventory.cs
@@ -0,0 +1,93 @@
+﻿namespace Sds.Inn;
+
+class Inventory
+{
+    void UpdateQuality()
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage Passes")
+            {
+                if (Items[i].Quality > 0)
+                {
+                    if (Items[i].Name != "Sulfuras")
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                    if (Items[i].Name == "Backstage Passes")
+                    {
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+            if (Items[i].Name != "Sulfuras")
+            {
+                Items[i].SellIn = Items[i].SellIn - 1;
+            }
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Name != "Aged Brie")
+                {
+                    if (Items[i].Name != "Backstage passes")
+                    {
+                        if (Items[i].Quality > 0)
+                        {
+                            if (Items[i].Name != "Sulfuras")
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    IList<Item> Items = new List<Item>
+    {
+        new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+        new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+        new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+        new Item { Name = "Sulfuras", SellIn = 0, Quality = 80 },
+        new Item { Name = "Backstage passes", SellIn = 15, Quality = 20 },
+        new Item { Name = "Conjured", SellIn = 3, Quality = 6 }
+    };
+}
+
+class Item
+{
+    public string Name { get; set; }
+    public int SellIn { get; set; }
+    public int Quality { get; set; }
+}
